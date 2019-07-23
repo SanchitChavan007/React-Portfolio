c5db09070d7d78539d9ccccb7efbc0c13d49e720
Sanchit Chavan <sanchitchavan@Sanchits-MacBook-Pro.local>
2019-07-23 02:25:13 -0400

    Final Changes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -19,7 +19,7 @@ export default class Header extends Component {
         <div className="row banner">
           <div className="banner-text">
             <h1 className="responsive-headline">I'm Sanchit Chavan</h1>
-            <h3>I'm a Boston based <span>software developer</span>, <span>mobile developer</span> and <span>web designer</span> creating awesome and
+            <h3 style ={{ color: "white" }} >I'm a Boston based <span>software developer</span>, <span>mobile developer</span> and <span>web designer</span> creating awesome and
               effective visual identities for companies of all sizes around the globe. Let's <a className="smoothscroll" href="#about">start scrolling </a>
               and learn more <a className="smoothscroll" href="#about">about me</a>.</h3>
             <hr />
