7e56e5bafe3d41ad5e045bf4f600c34f2adf76b9
Sanchit Chavan <sanchitchavan@Sanchits-MacBook-Pro.local>
2019-07-23 01:19:23 -0400

    Changes to deploy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import About from './components/about/about';
 import Resume from './components/resume/resume';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
+import resumeData from '../src/resumeData';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header resumeData={resumeData}/>
         <About />
         <Resume />
         <Contact />
---
+++ src/resumeData.js
@@ -0,0 +1,6 @@
+let resumeData = {
+    "imagebaseurl":"https://SanchitChavan007.github.io/",
+    "name": "Sanchit Chavan",
+    "role": "Software Developer"
+    }
+  export default resumeData
